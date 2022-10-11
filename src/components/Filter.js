0f08c2cd7997b5159118ec0f37e840612683b3b2
edmsimp <63891357+edmsimp@users.noreply.github.com>
2022-10-11 17:27:04 -0300

    Final adjustments

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -3,23 +3,23 @@ import React from 'react';
 import './Filter.css';
 
 function Filter ({ value, onValueChange, title, options}) {
-    const handleChange = event => {
-        onValueChange({ value: event.target.value });
-    }
+  const handleChange = event => {
+  	onValueChange({ value: event.target.value });
+  }
 
-    return (
-        <div>
-            <h4>{title}</h4>
-            <select
-            value={value}
-            onChange={handleChange}
-            >
-                {options.map(option => (
-                    <option key={option} >{option}</option>
-                ))}
-            </select>
-        </div>
-    );
+  return (
+    <div>
+      <h4>{title}</h4>
+      <select
+        value={value}
+        onChange={handleChange}
+      >
+        {options.map(item => (
+          <option key={item} >{item}</option>
+        ))}
+      </select>
+    </div>
+  );
 }
 
 export default Filter;
